06c34a50a20dc7b234d0cd7dddf794f59e3ffac6
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-09-05 18:31:57 -0600

    staring project with dropzone

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'DropzoneController@index');
+
+Route::get('dropzone', 'DropzoneController@index');
+
+Route::post('dropzone/upload', 'DropzoneController@upload')->name('dropzone.upload');
+
+Route::get('dropzone/fetch', 'DropzoneController@fetch')->name('dropzone.fetch');
+
+Route::get('dropzone/delete', 'DropzoneController@delete')->name('dropzone.delete');
